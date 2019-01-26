4a36782300f7c099331a870720eea9a4bcdeaff0
Olowu Emmanuel <emmanuelr20@yahoo.com>
2019-01-26 12:12:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
+import Products from './components/ProductList';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import {  BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { isAuthenticated } from './repository';
+
 
 class App extends Component {
+
+  logOut(){
+    localStorage.removeItem('x-access-token');
+  }
+  
   render() {
+    const auth = isAuthenticated();
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+              <Link className="navbar-brand" to="/">ShoppingCart</Link>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <Link className="nav-item nav-link" to="/">Products</Link>
+                  <Link className="nav-item nav-link" to="/cart">Cart</Link>
+                  { (auth) ? <Link className="nav-item nav-link" to="/checkout">Checkout</Link>: ''}
+                  {
+                    ( auth ) ? 
+                      ( <a className="nav-item nav-link" href="/" onClick={this.logOut}>Log out</a>) : 
+                      ( <Link className="nav-item nav-link float-right" to="/login">Log in</Link> )
+                   }
+                </div>
+              </div>
+            </div>
+          </nav>
+          <div className="container">
+            <br/>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/checkout" component={Checkout} />
+            { (!auth) ? <Route exact path="/login" component={Login} /> : '' }
+          </div>
+        </div>
+      </Router>
     );
   }
 }
